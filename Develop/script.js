c3392a0f7bd8507f642259e21dcddd1390d294a2
Aju Thomas <atajuapache@gmail.com>
2022-09-20 14:10:14 +0930

    working on generate password function and added numeric value to the input strings

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//making a function for generate password
+function generatePassword(){
+
+  var inputNumber = prompt("-	how many characters would you like your password to contain..?");
+
+ // changed  the string to a number
+  var changeToNumber = parseInt(inputNumber);
+  alert(changeToNumber);
+
+ // creating an if statement if user enter other than a number..
+
+  if ( isNaN(changeToNumber) ) {
+    alert("Fool, enter a number!");
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
