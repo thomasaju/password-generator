fcecb8b1d1272754976c46ecc11ceb0c96d268aa
Aju Thomas <atajuapache@gmail.com>
2022-09-21 15:06:03 +0930

    added options variable and if statements for the user perefernce true/false

--- Develop/script.js
+++ Develop/script.js
@@ -36,17 +36,45 @@ function generatePassword(){
   var numbers = ["1","2","3","4","5","6","7","8","9"];
   var symbolicValue = ["!" ,"@", "#", "$", "%", "^", "&", "*", "(", ")"];
   var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCaseLetter =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var lowerCaseLetters =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
+  var options = [lowerCaseLetters, upperCaseLetters ,numbers, symbolicValue];
 
 
+  
+  if(lowerCase === true){
+    options.push(lowerCaseLetters);
+  }
 
+  if(upperCase === true){
+    options.push(upperCaseLetters);
+  }
 
+  if(numeric === true){
+    options.push(numbers);
+  }
+  if(symbols === true){
+    options.push(symbolicValue);
+  }
 
-
+console.log(options);
 
 }
+
+var genereatedValues = ""
+
+for(var i =0; i < inputNumber; i++){
+
+}
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
